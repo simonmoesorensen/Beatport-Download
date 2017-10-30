91492b416ad598341266a02edfb257418c2806eb
Simon Moe <moe.simon@gmail.com>
2017-10-30 02:45:30 +0100

    Bug fixes and apperances
    Removed extra tabs, made exceptions for NoSuchElement errors, added download path to Desktop, added supportability for genres and a welcome message

--- main.py
+++ main.py
@@ -2,31 +2,41 @@
 Created on Sun Oct 29 18:19:06 2017
 
 INPUT:
-    songList
+    A html link from a beatport top 100 playlist
     
 OUTPUT:
-    songList: a list of strings with song names
-    
-USAGE:
-    songList = getHTML(link)
+    A folder in Desktop with the songs from the html link
 
 @Author: Simon Moe Sørensen, moe.simon@gmail.com
 """
 
-from functions import getHTML
+from getHTML import getHTML
 from selenium import webdriver
+from selenium.webdriver.remote.errorhandler import NoSuchElementException
 import time
-import os
+import os,sys
 
-songList = getHTML(input("Please enter the beatport top 100 link: "))
+
+print("""
+=========================================
+   Welcome to the Beatport downloader
+
+ Please sort the files after downloading
+ some could be corrupted or poor quality
+(That's what we get for not buying songs)
+=========================================""")
+
+songList,genre = getHTML(input("\nPlease enter the Beatport Top 100 link: "))
 
 #Set directory
-dir = os.path.dirname(os.path.realpath(__file__))
+dir = os.path.dirname(os.path.realpath(sys.argv[0]))
  
 #Set extensions path
 ext = os.path.join(dir, "chromeExt")
- 
 adblock = os.path.join(ext, "adblock.crx")
+
+#Set download path
+desktop = os.path.expanduser("~\Desktop\Beatport {}".format(genre))
  
 #Setting options
 chop = webdriver.ChromeOptions()
@@ -34,7 +44,7 @@ chop = webdriver.ChromeOptions()
 chop.add_extension(adblock) #Extension
 chop.add_argument("--mute-audio") #No audio, just in case
 chop.add_experimental_option("prefs", {
-  "download.default_directory": dir + "\Beatport Top 100",
+  "download.default_directory": desktop,
   "download.prompt_for_download": False,
   "download.directory_upgrade": True,
   "safebrowsing.enabled": True
@@ -42,26 +52,47 @@ chop.add_experimental_option("prefs", {
 
 #Setup browser driver with options
 driver = webdriver.Chrome(chrome_options = chop)
+driver.set_window_size(429,551)
 
 driver.get("https://www.mp3juices.cc/")
+
+#Remove extra tabs
+tabs = driver.window_handles
+driver.switch_to_window(tabs[1])
+driver.close()
+driver.switch_to_window(tabs[0])
 
 #Run through each element in songList
 for song in songList:
     
-    searchBar = driver.find_element_by_id("query") #Find search bar
+    try:
+        print("Downloading: ",song)
+        
+        searchBar = driver.find_element_by_id("query") #Find search bar
+        
+        searchBar.send_keys(song) #Type in song name
+        
+        driver.find_element_by_tag_name("button").click() #Click search
+        
+        time.sleep(1.3) #wait
+        
+        driver.find_element_by_link_text("Download").click() #Click download
+        
+        time.sleep(1.3) #wait
+        
+        driver.find_element_by_link_text("Download").click() #Again...
+        
+        searchBar.clear() #Clear search bar
+        
+        #Check if stupid add tab opened
+        if len(driver.window_handles)>1:
+           tabs = driver.window_handles
+           driver.switch_to_window(tabs[1])
+           driver.close()
+           driver.switch_to_window(tabs[0])
+           
+    except NoSuchElementException:
+        print("Download failed, moving on")
+        
     
-    searchBar.send_keys(song) #Type in song name
-    
-    driver.find_element_by_tag_name("button").click() #Click search
-    
-    time.sleep(1) #wait
-    
-    driver.find_element_by_link_text("Download").click() #Click download
-    
-    time.sleep(1) #wait
-    
-    driver.find_element_by_link_text("Download").click() #Again...
-    
-    searchBar.clear() #Clear search bar
-          
 driver.quit()
