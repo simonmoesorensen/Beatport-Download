2c266f288167923706cc0cb27de91ba7a1c9ef99
Simon Moe <moe.simon@gmail.com>
2017-11-03 02:15:30 +0100

    Spotify supportability and better error handling

--- main.py
+++ main.py
@@ -10,25 +10,48 @@ OUTPUT:
 @Author: Simon Moe Sørensen, moe.simon@gmail.com
 """
 
-from getHTML import getHTML
+from getHTML import getBeatport,getSpotify
 from selenium import webdriver
-from selenium.webdriver.remote.errorhandler import NoSuchElementException, WebDriverException
+from selenium.webdriver.remote.errorhandler import StaleElementReferenceException, NoSuchElementException, WebDriverException, TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import os,sys
 from sys import platform as _platform
 
 
 print("""
-=========================================
-   Welcome to the Beatport downloader
+==========================================
+Welcome to the Beatport/spotify downloader
+   
+   Your link should consists of a table
+   of songs on beatport or spotify
+   
+   Starting url examples:
+   - www.beatport.com/label/
+   - www.beatport.com/chart/
+   - www.beatport.com/name/
+   - www.beatport.com/release/
+   - open.spotify.com/user/spotify/playlist/
 
  Please sort the files after downloading
  some could be corrupted or poor quality
 (That's what we get for not buying songs)
 =========================================""")
 
-songList,genre = getHTML(input("\nPlease enter the Beatport Top 100 link: "))
+print("\n1. Spotify \n2. Beatport")
+input_ = int(input("Please pick a platform: "))
 
+if input_ == 1:
+    songList,name = getSpotify(input("\nPlease enter the Spotify playlist link: "))
+    platform = "Spotify"
+
+elif input_ == 2:    
+    songList,name = getBeatport(input("\nPlease enter the Beatport link: "))
+    platform = "Beatport"
+    
+    
 #Set directory
 dir = os.path.dirname(os.path.realpath(sys.argv[0]))
  
@@ -40,12 +63,12 @@ adblock = os.path.join(ext, "adblock.crx")
 if _platform == "darwin":
     # MAC OS X
     cDriver = "/chromedriver" #Set chromedriver path
-    desktop = os.path.expanduser("~/Desktop/Beatport {}".format(genre)) #Set download path
+    desktop = os.path.expanduser("~/Desktop/{} {}".format(platform,name)) #Set download path
     
 elif (_platform == "win32") or (_platform == "win64"):
     # Windows
     cDriver = "/chromedriver.exe" #Set chromedriver path
-    desktop = os.path.expanduser("~\Desktop\Beatport {}".format(genre)) #Set download path
+    desktop = os.path.expanduser("~\Desktop\{} {}".format(platform,name)) #Set download path
 
 #Setting options
 chop = webdriver.ChromeOptions()
@@ -72,9 +95,11 @@ driver.switch_to_window(tabs[1])
 driver.close()
 driver.switch_to_window(tabs[0])
 
-#Run through each element in songList
-for song in songList:
-    
+#Initial variables
+failed = []
+wait = 1
+
+for song in songList:    
     try:
         print("Downloading: ",song)
         
@@ -84,14 +109,39 @@ for song in songList:
         
         driver.find_element_by_tag_name("button").click() #Click search
         
-        time.sleep(1.3) #wait
+        time.sleep(1) #Give script time to rest..
         
-        driver.find_element_by_link_text("Download").click() #Click download
+        #wait until we can click on download
+        download1 = WebDriverWait(driver, wait).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "div.options")));
         
-        time.sleep(1.3) #wait
+        download1.click();
         
-        driver.find_element_by_link_text("Download").click() #Again...
-        
+        #wait until we can click on download
+        while True:
+            try:
+                download2 = WebDriverWait(driver, 15).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "a.url")));
+                
+                download2.click();
+                break
+            
+            #Try again            
+            except TimeoutException:
+                driver.refresh()
+                
+                searchBar = driver.find_element_by_id("query") #Find search bar
+            
+                searchBar.send_keys(song) #Type in song name
+                
+                driver.find_element_by_tag_name("button").click() #Click search
+                
+                #wait until we can click on download
+                download1 = WebDriverWait(driver, 15).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "div.options")));
+                
+                download1.click();
+
         time.sleep(0.5)
         
         searchBar.clear() #Clear search bar
@@ -103,13 +153,25 @@ for song in songList:
            driver.close()
            driver.switch_to_window(tabs[0])
            
+    except StaleElementReferenceException:
+        driver.get("https://www.mp3juices.cc/")
+        print("StaleElementException, Download failed, moving on")
+        failed.append(song) 
+        
     except NoSuchElementException:
-        print("Download failed, moving on")
+        print("NoSuchElementException, Download failed, moving on")
+        failed.append(song)
         searchBar.clear()
         
     except WebDriverException:
-        print("Download failed, moving on")
+        print("WebDriverException, Download failed, moving on")
+        failed.append(song)
         searchBar.clear()
-        
     
+print("\nFinished downloading songs!")
+
+print("\nThe following songs failed:",failed)
+
+input("Type anything to quit... \n")
+
 driver.quit()
